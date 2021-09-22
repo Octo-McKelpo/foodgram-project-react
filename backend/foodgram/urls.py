44f975cacedd6b45b0792f2b194874a2891f2edf
Octo-McKelpo <octopush1997@gmail.com>
2021-09-22 17:50:42 +0300

    Server Deploy 10

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,13 +1,8 @@
-import debug_toolbar
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("accounts/", include("users.urls")),
-    path("api/", include("api.urls"))
-]
-
-urlpatterns += [
-    path('__debug__/', include(debug_toolbar.urls)),
+    path("api/auth/", include("djoser.urls.authtoken")),
+    path("api/", include("api.urls", namespace='api'))
 ]
