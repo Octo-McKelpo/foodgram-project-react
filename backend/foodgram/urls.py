adf146aef8da708caf5a6c09ea736e041e7fb8ab
Octo-McKelpo <octopush1997@gmail.com>
2021-09-24 02:21:10 +0300

    Server Deploy 13

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('api/', include('api.urls')),
+    path('api/', include('users.urls')),
     path('admin/', admin.site.urls),
-    path('api/auth/', include('djoser.urls.authtoken')),
-    path('api/', include('recipes.urls')),
 ]
