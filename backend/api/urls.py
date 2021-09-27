cf3533713a3b77462cbc55dfce99d5bde94440ea
Octo-McKelpo <octopush1997@gmail.com>
2021-09-27 21:57:51 +0300

    Server Deploy 16

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (IngredientViewSet, RecipeViewSet, TagViewSet,
-                    CustomUserViewSet, PurchaseListView)
+                    CustomUserViewSet, PurchaseListView, FavoriteViewSet)
 
 app_name = 'api'
 
@@ -16,5 +16,7 @@ router.register('users', CustomUserViewSet, basename='users')
 urlpatterns = [
     path('', include(router.urls)),
     path('recipes/<int:recipe_id>/shopping_cart/',
-         PurchaseListView.as_view(), name='add_recipe_to_shopping_cart')
+         PurchaseListView.as_view(), name='add_recipe_to_shopping_cart'),
+    path('recipes/<int:recipe_id>/favorite/',
+         FavoriteViewSet.as_view(), name='add_recipe_to_favorite')
 ]
