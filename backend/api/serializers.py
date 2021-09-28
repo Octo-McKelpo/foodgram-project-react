ff2f65b86002e80a395068e2c65e390938f9d9a1
Octo-McKelpo <octopush1997@gmail.com>
2021-09-28 22:08:08 +0300

    Server Deploy 17.2

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -97,9 +97,11 @@ class CreateRecipeSerializer(serializers.ModelSerializer):
         self.create_or_update(validated_data.get('ingredients'), instance)
         if validated_data.get('image') is not None:
             instance.image = validated_data.get('image')
-        instance.name = validated_data.get('name')
-        instance.text = validated_data.get('text')
-        instance.cooking_time = validated_data.get('cooking_time')
+        instance.name = validated_data.get('name', instance.name)
+        instance.text = validated_data.get('text', instance.text)
+        instance.cooking_time = validated_data.get('cooking_time',
+                                                   instance.cooking_time)
+        instance.image = validated_data.get('image', instance.image)
         instance.save()
         return instance
 
